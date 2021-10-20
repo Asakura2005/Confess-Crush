7772ec6f88d09eae11935fb7d516a5ee8f65f980
Asakura2005 <92841326+Asakura2005@users.noreply.github.com>
2021-10-20 12:46:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
     titleWeb: "Gửi choa...🥰",
-    introTitle: 'Trang ngố',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Yêu em không nè :33 ',
+    introTitle: 'Con gái yêu 🤣',
+    introDesc: `Con biết gì không bố iu con lắm 
+    ❤️❤️❤️❤️❤️❤️❤️`,
+    btnIntro: ' Tiếp đi ',
+    title: 'Nhìn cái gì mà nhìn yêu không mà nhìn ',
+    desc: 'Yêu bố không nè :33 ',
     btnYes: 'Yêu lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
-    btnReply: 'Nói Em Bt Đii <3',
+    question: 'Ỏ sao yêu bố vậy hihi 🤣',
+    btnReply: 'Ấn đây xong nói bố biết di',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333 ',
-    mess: ' Yêu nhiều nắm 😘😘',
-    messDesc: ' Tí cho em gõ đầu chị cái nha',
+    mess: 'Lớp ziu 🤣',
+    messDesc: 'Tí thi xong đi đâu bố đi ké vs nha',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/iam.cat411' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+}s
